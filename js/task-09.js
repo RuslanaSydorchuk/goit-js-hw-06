49e291164b2d6e0e8489b05ae36118f5c3ee9085
RuslanaSydorchuk <ruslanasydorchuk@gmail.com>
2022-09-18 21:06:46 +0300

    виконано 9 завдання

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btn = document.querySelector('.change-color');
+const refColor = document.querySelector('.color');
+
+btn.addEventListener('click', onBtn);
+
+function onBtn(evt) {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  refColor.textContent = newColor;
+}
