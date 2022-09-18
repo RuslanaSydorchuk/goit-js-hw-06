99aa0e293b578cf2d3e920d36dfae7f97c6fd7ea
RuslanaSydorchuk <ruslanasydorchuk@gmail.com>
2022-09-18 20:09:10 +0300

    виконано 8 завдання

--- js/task-07.js
+++ js/task-07.js
@@ -4,11 +4,3 @@ size.value = 16
 size.addEventListener('input', (evt) => {
     text.style.fontSize = size.value + 'px'
 })
-
-
-// const range = document.querySelector('#font-size-control')
-// const text = document.querySelector('#text')
-// range.value = 16
-// range.addEventListener('input', (event) => {
-//     text.style.fontSize = range.value + 'px'
-// })
