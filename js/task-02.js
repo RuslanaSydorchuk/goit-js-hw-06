e33e438a97b80b367c69e70d6852ff7e3276344c
RuslanaSydorchuk <104679795+RuslanaSydorchuk@users.noreply.github.com>
2022-09-19 19:37:28 +0300

    скорегувала 2 завдання

--- js/task-02.js
+++ js/task-02.js
@@ -19,16 +19,3 @@ for (let i = 0; i < ingredients.length; i+=1){
 
 list.append(...items);
 console.log(list)
-
-// const items =[];
-
-// for (let i=0; i < ingredients.length; i+= 1 ) {
-//   const ingredientsItemEl = document.createElement('li');
-//   ingredientsItemEl.textContent = ingredients[i];
-//   ingredientsItemEl.classList.add('.item')
-
-//   items.push(ingredientsItemEl)
-// }
-
-// const ingredientsListEl = document.querySelector('#ingredients')
-// ingredientsListEl.append(...items)
